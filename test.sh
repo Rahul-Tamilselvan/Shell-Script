7e738637287d6cedfb6780edef10da8b0b737974
kirubakaran <kirubakarankannan254@gmail.com>
2024-10-24 17:18:04 +0530

    Adding Grep command example

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+##########################
+# Name: Rahul
+# Date: 24-10-2024
+#
+# Grep command
+# Version: v1
+# ########################
+
+echo 1
+echo 11
+echo 12
+echo 55
+echo 77
+
+
+# ./test.sh | grep 1
